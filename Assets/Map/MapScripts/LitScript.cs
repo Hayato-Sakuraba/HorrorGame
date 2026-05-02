71e7da8b0609b8bfddaa6462e09fafd05b6a588d
Syuto256 <224196430+Syuto256@users.noreply.github.com>
2026-05-02 16:01:30 +0900

    部屋の明かりを追加
    部屋ごとの明かりをつけたり消したり出来るスクリプトを追加
    スクリプト内の関数を呼べばつけたり消したり出来ます

---
+++ Assets/Map/MapScripts/LitScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class LitScript : MonoBehaviour
+{
+    [Header("部屋オブジェクトを登録")]
+    [Tooltip("各部屋をドラッグ＆ドロップ")]
+    [SerializeField] private GameObject[] targetRooms;
+
+    //SetRoomVisibility(0, true);これで表示
+    //SetRoomVisibility(0, false);これで非表示
+        public void SetRoomVisibility(int index, bool isVisible)
+    {
+        
+        if (index < 0 || index >= targetRooms.Length)
+        {
+            Debug.LogError($"[RoomVisibilityController] エラー: 指定されたインデックス ({index}) は配列の範囲外です。");
+            return;
+        }
+
+        if (targetRooms[index] == null)
+        {
+            Debug.LogError($"[RoomVisibilityController] エラー: インデックス ({index}) のオブジェクトがInspectorで未設定（Null）です。");
+            return;
+        }
+
+        // 表示・非表示の切り替え
+        targetRooms[index].SetActive(isVisible);
+    }
+}
